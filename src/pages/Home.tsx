2d22d85cc833c39721aa2c15bc5fc6adbfb9b5e2
jordan-choi <yyoungbest@gmail.com>
2024-10-26 17:41:53 +0100

    feat: Add Progress to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,20 +2,27 @@ import { Layout } from "@/components/Layout";
 import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
+  // TODO: get this from state management library
+  const userName = "Anom";
+  // TODO: get these values from API
+  const progressValue = 40;
+  const userTitle = "Newcomer";
+  const leftPoints = 30;
+
   return (
     <Layout>
       <main>
         <div className="flex flex-col items-center  justify-center mx-auto">
-          <div className=" mt-12">
-            <h1 className="text-2xl md:text-4xl lg:text-4xl font-bold text-center">
-              You are now level
-            </h1>
+          <div className=" mt-12">Welcome back {userName}</div>
+          <div className="relative w-[60%]">
+            <Progress value={progressValue} className="h-[2.4rem]" />
+            <div className="absolute inset-0 flex items-center justify-center">
+              <span className="text-xs font-medium text-primary-foreground drop-shadow">
+                {progressValue}% {userTitle}
+              </span>
+            </div>
           </div>
-
-          <div className="w-2/4  mx-auto mt-12">
-            <Progress value={50} />
-            <span> 50 / 100</span>
-          </div>
+          Get {leftPoints} more points to level up
         </div>
       </main>
     </Layout>
