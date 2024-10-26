07bd2b468a2183c09b32c9939629d61469ca8590
jordan-choi <yyoungbest@gmail.com>
2024-10-26 18:38:55 +0100

    feat: Add UserStore

---
+++ src/store.ts
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+interface UserState {
+  name: string;
+  id: string;
+}
+
+export const useUserStore = create<UserState>()(() => ({
+  name: "",
+  id: "",
+}));
