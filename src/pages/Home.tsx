e50af7c14222bfb1d00929e40ed2577203392a67
PDochev <plamendochev0@gmail.com>
2024-10-26 14:31:30 +0100

    installed tailwindcss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
     </div>
   );
 }
