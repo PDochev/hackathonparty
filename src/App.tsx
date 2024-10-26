d26ed39098e1403b5f57e8dbe0c3bceef95b9e1d
jordan-choi <yyoungbest@gmail.com>
2024-10-26 23:44:56 +0100

    feat: Add ReactQueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import Badges from "./pages/Badges";
 import Files from "./pages/Files";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -15,7 +19,7 @@ function App() {
           <Route path="/files" element={<Files />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
