7b365b794aa0e5aca0ac861a4f9ae66a5e9007b2
PDochev <plamendochev0@gmail.com>
2024-10-26 15:54:46 +0100

    Login page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,31 @@
 import Navbar from "@/components/custom/Navbar";
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
-    <header>
-      <nav role="navigation">
-        <Navbar>
-          <div className="flex items-center justify-between w-full m-2">
-            <h4 className="ml-4 text-md font-semibold tracking-tight scroll-m-20 ">
-              Immigration App
-            </h4>
-            <div className="flex items-center gap-4 mr-4"></div>
-          </div>
-        </Navbar>
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav role="navigation">
+          <Navbar>
+            <div className="flex items-center justify-between w-full m-2">
+              <h4 className="ml-4 text-md font-semibold tracking-tight scroll-m-20 ">
+                Immigration App
+              </h4>
+              <div className="flex items-center gap-4 mr-4"></div>
+            </div>
+          </Navbar>
+        </nav>
+      </header>
+      <main>
+        <div className="flex  items-center justify-center mt-12">
+          <h1 className="text-4xl font-bold">Welcome to the Immigration app</h1>
+        </div>
+
+        <div className="w-2/4 flex items-center justify-center mx-auto mt-12">
+          <Progress value={50} />
+          <span> 50 / 100</span>
+        </div>
+      </main>
+    </>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,52 @@
-export default function Login() {
+import login_img from "@/assets/login.jpg";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Link, redirect } from "react-router-dom";
+
+function Login() {
   return (
-    <div>
-      <h1 className="">Login</h1>
+    <div role="presentation" className="flex w-full  min-h-svh">
+      <section className="hidden w-1/2  lg:flex ">
+        <img
+          className="w-full  h-svh object-cover bg-center flex-shrink-0"
+          src={login_img}
+          alt="Image of a person reading a book."
+        />
+      </section>
+      <section className="lg:w-1/2 flex items-center justify-center mx-auto ">
+        <form className="w-72 md:w-96 py-6 mx-auto flex flex-col justify-center items-center border rounded-md  ">
+          <h3 className="scroll-m-20 text-1xl font-semibold tracking-tight">
+            Immigration
+          </h3>
+          <h2 className="scroll-m-20  pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-4">
+            Welcome Back
+          </h2>
+
+          <div className="grid w-3/4 max-w-sm items-center gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" id="email" placeholder="Email" required />
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <Button type="submit" className="w-3/4 mt-4">
+            Login
+          </Button>
+          <p className="text-sm leading-7 [&:not(:first-child)]:mt-6">
+            Don't have an account ?
+            <Link className="text-blue-600 ml-2" to="/register">
+              Create account
+            </Link>
+          </p>
+        </form>
+      </section>
     </div>
   );
 }
+
+export default Login;
