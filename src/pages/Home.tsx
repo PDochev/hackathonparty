1491e3a0b7031c3325f1949ae3485d885b2f4b33
jordan-choi <yyoungbest@gmail.com>
2024-10-26 17:49:39 +0100

    feat: Add ExpProgress component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
+import { ExpProgress } from "@/components/business/ExpProgress";
 import { Layout } from "@/components/Layout";
-import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   // TODO: get this from state management library
@@ -12,17 +12,13 @@ export default function Home() {
   return (
     <Layout>
       <main>
-        <div className="flex flex-col items-center  justify-center mx-auto">
-          <div className=" mt-12">Welcome back {userName}</div>
-          <div className="relative w-[60%]">
-            <Progress value={progressValue} className="h-[2.4rem]" />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <span className="text-xs font-medium text-primary-foreground drop-shadow">
-                {progressValue}% {userTitle}
-              </span>
-            </div>
-          </div>
-          Get {leftPoints} more points to level up
+        <div className="flex flex-col items-center justify-center mx-auto">
+          <div className="mt-12">Welcome back {userName}</div>
+          <ExpProgress
+            value={progressValue}
+            userTitle={userTitle}
+            leftPoints={leftPoints}
+          />
         </div>
       </main>
     </Layout>
