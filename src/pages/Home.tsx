b9ff530e8b9411fe0305b854d6bd42f3c91445fa
PDochev <plamendochev0@gmail.com>
2024-10-26 14:34:39 +0100

    installed shadcn

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello world!
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
