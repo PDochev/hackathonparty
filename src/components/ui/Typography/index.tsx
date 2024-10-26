54adc117c41b157c422e5526b619e25a8f82a91f
jordan-choi <yyoungbest@gmail.com>
2024-10-26 21:34:38 +0100

    feat: Add Typography

---
+++ src/components/ui/Typography/index.tsx
@@ -0,0 +1,11 @@
+import { TypographyH1 as H1 } from "./H1";
+import { TypographyH2 as H2 } from "./H2";
+import { TypographyH3 as H3 } from "./H3";
+import { TypographyH4 as H4 } from "./H4";
+
+export const Typography = {
+  H1,
+  H2,
+  H3,
+  H4,
+};
