bad9576aa1c50f7390025c8ceb46b5fb5a976f62
clarkanastasia <clarkanastasia@outlook.com>
2024-10-27 09:46:25 +0000

    update instruction

--- src/components/business/ExpProgress.tsx
+++ src/components/business/ExpProgress.tsx
@@ -18,7 +18,7 @@ export function ExpProgress({ userTitle, value, leftPoints }: Props) {
         </div>
       </div>
       <div className="mt-3 text-center">
-        Get {leftPoints} more points to level up
+        Complete {leftPoints} more tasks to level up
       </div>
     </div>
   );
