cf2dc2c45d707dfaead766040ab3fbb54ea77d7c
jordan-choi <yyoungbest@gmail.com>
2024-10-27 10:58:27 +0000

    fix: Revert Upcoming Tasks UI

--- src/components/business/TaskCard/index.tsx
+++ src/components/business/TaskCard/index.tsx
@@ -1,7 +1,8 @@
 import { Task } from "@/types";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { TodoItem } from "@/components/ui/TodoItem";
-
+import success_img from "@/assets/success.svg";
+import { ChevronDown } from "lucide-react";
 interface Props {
   completed: boolean;
   tasks: Task[];
@@ -11,14 +12,24 @@ interface Props {
 export function TaskCard({ completed, tasks, onToggleSubTasks }: Props) {
   if (completed || tasks.length === 0) {
     return (
-      <Card className="w-[350px] mx-auto bg-gray-100">
-        <CardHeader>
-          <CardTitle>You're all caught up!</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <span className="leading-7 w-fit">Nothing to do here</span>
-        </CardContent>
-      </Card>
+      <div className="flex flex-col items-center gap-2">
+        <Card className="w-[350px] bg-gray-100 mt-4">
+          <CardHeader>
+            <CardTitle className="text-center">You're all caught up!</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col items-center justify-center">
+            <img src={success_img} alt="success" className="w-64 h-64 m-auto" />
+            <span className="leading-7 w-fit ">Nothing to do here</span>
+          </CardContent>
+        </Card>
+        <Card className="w-[350px] bg-gray-100">
+          <CardHeader>
+            <CardTitle className="flex justify-between items-center">
+              Upcoming Tasks <ChevronDown className="h-4 w-4" />
+            </CardTitle>
+          </CardHeader>
+        </Card>
+      </div>
     );
   }
 
